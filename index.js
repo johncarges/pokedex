e58c767d57c0925b617253d171f7583a6215cc4f
Tess <tess.merrill25@gmail.com>
2023-07-24 15:47:37 -0600

    added partial function to render featured pokemon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pokemonUrl = baseUrl + "/pokemon"
 
 const featuredPokemonWindow = document.getElementById("featured-pokemon")
 const pokemonList = document.getElementById('pokemon-list')
-
+const pokemonFeatured = document.getElementById('featured-pokemon')
 
 
 // INITIAL FETCH
@@ -14,6 +14,35 @@ fetch(pokemonUrl)
 .then((data) => {
     data.slice(0,10).forEach(renderPokemonInList)
 })
+
+// Function to render feature Pokemon at top of page
+
+const renderFeaturePokemon = pokemon => {
+    // Insert image based on API
+    // let image = document.createElement('img')
+    // image.src = pokemon.image
+    // image.alt = pokemon.name
+    // image.className = "pokemon-image"
+    // pokemonFeatured.appendChild(image)
+
+    // Insert Pokemon name
+    let h2 = document.createElement('h2')
+    h2.textContent = pokemon.name.english
+    h2.id = 'feature-name'
+    pokemonFeatured.appendChild(h2)
+
+    // Insert Pokedex number
+    let h3 = document.createElement('h3')
+    h3.textContent = `Pokedex Number: ${pokemon.id}`
+    h3.id = 'pokedex-num'
+    pokemonFeatured.appendChild(h3)
+
+    // Insert Type info
+    // Insert caught status
+    // Insert Notes
+
+
+}
 
 
 function renderPokemonInList(pokemon) {
